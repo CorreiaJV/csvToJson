d1ad8a6aec82effbbbf8e3ed4e7ca6fc99a34ac0
CorreiaJV <correia.jv16@gmail.com>
2022-02-27 22:28:40 -0300

    add routes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,5 +1,25 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import TesteController from '../controller/teste';
 
 const router = Router();
+const testeController = new TesteController();
+
+
+router.post("/teste",(req: Request, res: Response) => {
+    testeController.downloadCsv(req,res);
+});
+
+router.get("/testeGetAll",(req: Request, res: Response) => {
+    testeController.getData(req,res);
+});
+router.get("/testeGetAllPageable",(req: Request, res: Response) => {
+    testeController.getDataPageable(req,res);
+});
+router.get("/testeGetFiltered",(req: Request, res: Response) => {
+    testeController.getDataFiltered(req,res,req.query);
+});
+router.delete("/delete",(req: Request, res: Response) => {
+    testeController.deleteAll(req,res);
+});
 
 export default router;
